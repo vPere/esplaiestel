0e47d91de01bd6a0751b8a70b26d99f2a7bc2fd3
vPere <pere.vidiella@tictap.me>
2023-09-25 18:56:39 +0200

    Changes things from header and adds mockup of main page.

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html>
 <head>
     <title>Esplai Estel de Barcelona</title>
-    <link rel="stylesheet" type="text/css" href="styles/css/main.css">
-    <script src="https://unpkg.com/scroll-out/dist/scroll-out.min.js"></script>
 </head>
     <?php include 'commons/header/header.html';?>
+    <?php include 'main.html';?>
+    <?php include 'commons/footer/footer.html';?>
 </html>
