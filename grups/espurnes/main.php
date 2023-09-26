d75dbb6524a911e0e27ec639de77a828b74ffb58
vPere <pere.vidiella@gmail.com>
2023-09-26 17:01:56 +0200

    Adds first version of Espurnes and Barrufets main pages. Fixes independent styles for each group that didn't work.

---
+++ grups/espurnes/main.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<!-- TODO: make header links absolute links to make them work everywhere -->
+    <?php include '../../commons/header/header.html';?>
+    <?php include 'espurnes.html';?>
+    <?php include '../../commons/footer/footer.html';?>
+</html>
