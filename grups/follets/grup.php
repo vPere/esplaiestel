4e4f58592815a5f8dbe0e844b851bacbbdc7b7bb
vPere <pere.vidiella@gmail.com>
2023-09-26 10:55:43 +0200

    Starts coding follets grup page.

---
+++ grups/follets/grup.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Follets - Esplai Estel de Barcelona</title>
+</head>
+<!-- TODO: make header links absolute links to make them work everywhere -->
+    <?php include '../../commons/header/header.html';?>
+    <?php include 'follets.php';?>
+    <?php include '../../commons/footer/footer.html';?>
+</html>
