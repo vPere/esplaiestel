085783418cf840c80a4623989ecb27d2ca0f3b76
vPere <pere.vidiella@gmail.com>
2023-09-26 16:45:01 +0200

    Adds first version of Follets main page.

--- grups/follets/main.php
+++ grups/follets/main.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Follets - Esplai Estel de Barcelona</title>
-</head>
 <!-- TODO: make header links absolute links to make them work everywhere -->
     <?php include '../../commons/header/header.html';?>
-    <?php include 'follets.php';?>
+    <?php include 'follets.html';?>
     <?php include '../../commons/footer/footer.html';?>
 </html>
